ff335491dc3d40ce84454b3e6466eeaf44047e03
Stephen Chang <3130100653@zju.edu.cn>
2017-11-25 15:57:19 -0500

    update interface

--- controllers/database.js
+++ controllers/database.js
@@ -2,180 +2,160 @@ const express = require('express');
 const router = express.Router();
 const { Pool, Client } = require('pg');
 const cryptType = 'bf';
-const connectionString = 'postgres://localhost:5432/gallery';
+const connectionString = 'hw://localhost:5432/gallery';
 
 const pool = new Pool({
     connectionString: connectionString,
 });
 
-function addArtist(name, url, address){
-    const shouldAbort = (err) => {
-        if (err) {
-            console.error('Error in addArtist', err.stack);
-            client.query('ROLLBACK', (err) => {
-                if (err) {
-                    console.error('Error rolling back client', err.stack);
-                }
-                // release the client back to the pool
-                done();
-            })
-        }
-        return !!err;
-    };
+function addArtist(artist_id, avator, name, address, self_intro, gender){
+    pool.connect((err, client, done) => {
+        if(err) throw err;
 
-    client.query('BEGIN', (err) => {
-        if (shouldAbort(err)) return;
+        const shouldAbort = (err) => {
+            if (err) {
+                console.error('Error in addArtist', err.stack);
+                client.query('ROLLBACK', (err) => {
+                    if (err) {
+                        console.error('Error rolling back client', err.stack);
+                    }
+                    // release the client back to the pool
+                    done();
+                })
+            }
+            return !!err;
+        };
 
-        const insertArtist = 'INSERT INTO Artist(name, url, address) VALUES($1, $2, $3)';
-        const insertValues = [name, url, address];
-        client.query(insertArtist, insertValues, (err, res) => {
+        client.query('BEGIN', (err) => {
             if (shouldAbort(err)) return;
 
-            client.query('COMMIT', (err) => {
-                if (err) {
-                    console.error('Error committing transaction: insert artist', err.stack);
-                }
-                done();
+            const insertArtist = 'INSERT INTO Artist(artist_id, avator, name, address, self_intro, gender) VALUES($1, $2, $3, $4, $5, $6)';
+            const insertValues = [artist_id, avator, name, address, self_intro, gender];
+            client.query(insertArtist, insertValues, (err, res) => {
+                if (shouldAbort(err)) return;
+
+                client.query('COMMIT', (err) => {
+                    if (err) {
+                        console.error('Error committing transaction: insert artist', err.stack);
+                    }
+                    done();
+                })
             })
-        })
+        });
     });
 }
 
-function addPainting(name, year, price){
-    const shouldAbort = (err) => {
-        if (err) {
-            console.error('Error in addPainting', err.stack);
-            client.query('ROLLBACK', (err) => {
-                if (err) {
-                    console.error('Error rolling back client', err.stack);
-                }
-                // release the client back to the pool
-                done();
-            })
-        }
-        return !!err;
-    };
+function addPainting(painting_id, artist_id, price, height, width, img, title, type){
+    pool.connect((err, client, done) => {
+        if (err) throw err;
 
-    client.query('BEGIN', (err) => {
-        if (shouldAbort(err)) return;
+        const shouldAbort = (err) => {
+            if (err) {
+                console.error('Error in addPainting', err.stack);
+                client.query('ROLLBACK', (err) => {
+                    if (err) {
+                        console.error('Error rolling back client', err.stack);
+                    }
+                    // release the client back to the pool
+                    done();
+                })
+            }
+            return !!err;
+        };
 
-        const insertPainting = 'INSERT INTO Painting(name, year, price) VALUES($1, $2, $3)';
-        const insertValues = [name, year, price];
-        client.query(insertPainting, insertValues, (err, res) => {
+        client.query('BEGIN', (err) => {
             if (shouldAbort(err)) return;
 
-            client.query('COMMIT', (err) => {
-                if (err) {
-                    console.error('Error committing transaction: insert painting', err.stack);
-                }
-                done();
+            const insertPainting = 'INSERT INTO Painting(painting_id, artist_id, price, height, width, img, title, type) VALUES($1, $2, $3, $4, $5, $6, $7, $8)';
+            const insertValues = [painting_id, artist_id, price, height, width, img, title, type];
+            client.query(insertPainting, insertValues, (err, res) => {
+                if (shouldAbort(err)) return;
+
+                client.query('COMMIT', (err) => {
+                    if (err) {
+                        console.error('Error committing transaction: insert painting', err.stack);
+                    }
+                    done();
+                })
             })
-        })
+        });
     });
 }
 
 function addCustomer(name, email, password){
-    const shouldAbort = (err) => {
-        if (err) {
-            console.error('Error in addCustomer', err.stack);
-            client.query('ROLLBACK', (err) => {
-                if (err) {
-                    console.error('Error rolling back client', err.stack);
-                }
-                // release the client back to the pool
-                done();
-            })
-        }
-        return !!err;
-    };
+    pool.connect((err, client, done) => {
+        if (err) throw err;
 
-    client.query('BEGIN', (err) => {
-        if (shouldAbort(err)) return;
+        const shouldAbort = (err) => {
+            if (err) {
+                console.error('Error in addCustomer', err.stack);
+                client.query('ROLLBACK', (err) => {
+                    if (err) {
+                        console.error('Error rolling back client', err.stack);
+                    }
+                    // release the client back to the pool
+                    done();
+                })
+            }
+            return !!err;
+        };
 
-        const insertCustomer = 'INSERT INTO Customer(name, email, password) VALUES($1, $2, crypt($3, gen_salt($4)))';
-        const insertValues = [name, year, price, cryptType];
-        client.query(insertCustomer, insertValues, (err, res) => {
+        client.query('BEGIN', (err) => {
             if (shouldAbort(err)) return;
 
-            client.query('COMMIT', (err) => {
-                if (err) {
-                    console.error('Error committing transaction: insert painting', err.stack);
-                }
-                done();
+            const insertCustomer = 'INSERT INTO Customer(name, email, password) VALUES($1, $2, crypt($3, gen_salt($4)))';
+            const insertValues = [name, email, password, cryptType];
+            client.query(insertCustomer, insertValues, (err, res) => {
+                if (shouldAbort(err)) return;
+
+                client.query('COMMIT', (err) => {
+                    if (err) {
+                        console.error('Error committing transaction: insert painting', err.stack);
+                    }
+                    done();
+                })
             })
-        })
-    });
-}
-
-function addHave(artist_id, painting_id){
-    const shouldAbort = (err) => {
-        if (err) {
-            console.error('Error in addHave', err.stack);
-            client.query('ROLLBACK', (err) => {
-                if (err) {
-                    console.error('Error rolling back client', err.stack);
-                }
-                // release the client back to the pool
-                done();
-            })
-        }
-        return !!err;
-    };
-
-    client.query('BEGIN', (err) => {
-        if (shouldAbort(err)) return;
-
-        const insertHave = 'INSERT INTO Have(artist_id, painting_id) VALUES($1, $2)';
-        const insertValues = [artist_id, painting_id];
-        client.query(insertHave, insertValues, (err, res) => {
-            if (shouldAbort(err)) return;
-
-            client.query('COMMIT', (err) => {
-                if (err) {
-                    console.error('Error committing transaction: insert buy', err.stack);
-                }
-                done();
-            })
-        })
+        });
     });
 }
 
 function addBuy(customer_id, painting_id){
-    const shouldAbort = (err) => {
-        if (err) {
-            console.error('Error in addBuy', err.stack);
-            client.query('ROLLBACK', (err) => {
-                if (err) {
-                    console.error('Error rolling back client', err.stack);
-                }
-                // release the client back to the pool
-                done();
-            })
-        }
-        return !!err;
-    };
+    pool.connect((err, client, done) => {
+        if (err) throw err;
 
-    client.query('BEGIN', (err) => {
-        if (shouldAbort(err)) return;
+        const shouldAbort = (err) => {
+            if (err) {
+                console.error('Error in addBuy', err.stack);
+                client.query('ROLLBACK', (err) => {
+                    if (err) {
+                        console.error('Error rolling back client', err.stack);
+                    }
+                    // release the client back to the pool
+                    done();
+                })
+            }
+            return !!err;
+        };
 
-        const insertBuy = 'INSERT INTO Buy(customer_id, painting_id) VALUES($1, $2)';
-        const insertValues = [customer_id, painting_id];
-        client.query(insertBuy, insertValues, (err, res) => {
+        client.query('BEGIN', (err) => {
             if (shouldAbort(err)) return;
 
-            client.query('COMMIT', (err) => {
-                if (err) {
-                    console.error('Error committing transaction: insert buy', err.stack);
-                }
-                done();
+            const insertBuy = 'INSERT INTO Buy(customer_id, painting_id) VALUES($1, $2)';
+            const insertValues = [customer_id, painting_id];
+            client.query(insertBuy, insertValues, (err, res) => {
+                if (shouldAbort(err)) return;
+
+                client.query('COMMIT', (err) => {
+                    if (err) {
+                        console.error('Error committing transaction: insert buy', err.stack);
+                    }
+                    done();
+                })
             })
-        })
+        });
     });
 }
 
-
-
 pool.end();
-
 
 module.exports = router;

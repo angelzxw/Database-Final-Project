b079a953380eb6acbe22d206980914d444f11e5c
Kai <3130000495@zju.edu.cn>
2017-11-25 17:37:26 -0500

    add update artworks

--- crawler/updateDatabase.js
+++ crawler/updateDatabase.js
@@ -5,14 +5,30 @@
 const { addArtist,addPainting }  = require("../controllers/database");
 var fs = require('fs');
 
-fs.readFile("./artists.json","utf8",function (err,data) {
-   if(err)
-       throw err;
-   const artists = JSON.parse(data);
-   for(let artist of artists){
-	//console.log(artist);
-       addArtist(artist["artist_id"],artist["avator"],artist["name"],artist["address"],artist["self_intro"],artist["Gender"]);
-   }
-});
+function updateArtists() {
+    fs.readFile("./artists.json","utf8",function (err,data) {
+        if(err)
+            throw err;
+        const artists = JSON.parse(data);
+        for(let artist of artists){
+            //console.log(artist);
+            addArtist(artist["artist_id"],artist["avator"],artist["name"],artist["address"],artist["self_intro"],artist["Gender"]);
+        }
+    });
+}
 
+function updateArtworks() {
+    fs.readFile("./artworks.json","utf8",function (err,data) {
+        if(err)
+            throw err;
+        const artworks = JSON.parse(data);
+        for(let artwork of artworks){
+            //console.log(artist);
+            const height = artwork["height"].replace("\"","");
+            const width = artwork["width"].replace("\"","");
+            addPainting(artwork["product_id"],artwork["artist_id"],artwork["price"],height,width,artwork["img"],artwork["title"],artwork["type"]);
+        }
+    });
+}
+updateArtworks();
 
